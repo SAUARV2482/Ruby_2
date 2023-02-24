2da11590713b9fca26724cf6350961026f8ab66e
Brahmbhatt saurav <saurav.brahmbhatt@bacancy.com>
2023-02-24 14:54:17 +0530

    Reverse only vowels in the given string

---
+++ third.rb
@@ -0,0 +1,30 @@
+class Reverse
+  def isVowel(character)
+    character == "a" ||character ==  "e" ||character ==  "i" ||character ==  "o" ||character ==  "u" 
+  end
+  def reverse_vowel
+    string = "communication"
+    len = string.length
+    i=0
+    j= len-1
+    while i<j
+      if(!isVowel(string[i]))
+        i+=1
+        next
+      end
+      if(!isVowel(string[j]))
+        j-=1
+        next
+      end
+      temp=""
+      temp = string[i]
+      string[i]=string[j]
+      string[j] = temp
+      i+=1
+      j-=1
+    end
+    p string
+  end
+end
+r1 = Reverse.new
+r1.reverse_vowel
