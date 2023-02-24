73c11c96bdfb08ebd181e3c54023f86d59deb47b
Brahmbhatt saurav <saurav.brahmbhatt@bacancy.com>
2023-02-24 14:59:58 +0530

    Convert a hash into an array

---
+++ six.rb
@@ -0,0 +1,21 @@
+# Convert a hash into an array. Nothing more, Nothing less.
+# Input - {name: 'Jeremy', age: 24, role: 'Software Engineer'}
+
+  
+  class Main
+    def hash_to_array
+      input = {name: 'Jeremy', age: 24, role: 'Software Engineer'}
+      # p input.to_a    
+       arr=[]
+       i=0
+       input.each { |key,value|
+        arr.push([key.to_s, value])
+        i +=1
+       }
+      print arr
+    end
+  end
+
+
+f1= Main.new()
+f1.hash_to_array
